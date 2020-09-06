7583d6d7766e322e55ddab2f83cf9e7afaf5965b
wesley <wesleyyw8@gmail.com>
2020-09-06 13:46:56 -0300

    first page

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  // {
+  //   path: 'item/:id',
+  //   component: CharacterEditComponent
+  // },
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full',
+  }, {
+    path: '**', redirectTo: 'home'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
